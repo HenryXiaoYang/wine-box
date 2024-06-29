9a6e10e2f55a7ef676608b71dc1fa9966d0ff975
Henry Yang <83214045+HenryXiaoYang@users.noreply.github.com>
2024-06-29 12:13:11 +0800

    docs: Update the comment to download 64bit of Python3.8

--- root/init/001-install-wine-python3.8.sh
+++ root/init/001-install-wine-python3.8.sh
@@ -12,7 +12,7 @@ function pyok() {
     done
 }
 pyok &
-# download python-3.8.10.exe
+# download python-3.8.10-amd64.exe
 wget https://www.python.org/ftp/python/3.8.10/python-3.8.10-amd64.exe
 # start install
 wine start python-3.8.10-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
